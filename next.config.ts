310eb287e9250acd842c66e79a383b89ee3cefb7
Amr Ezzat <amr.2023001640@buc.edu.eg>
2024-11-23 01:11:26 +0200

    ttt

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100MB'
+    },
+  },
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
